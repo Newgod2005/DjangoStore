fd3426a5f15833d4a74fc79d150e886f340db933
VladNevgod <vladnewgod@gmail.com>
2023-07-08 11:57:52 +0300

    Product model has been created

--- products/models.py
+++ products/models.py
@@ -7,3 +7,11 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
+class Product(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    #Цифры до запятой и после запятой. DecimalField - тип данных для цены
+    quantity =models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
